396c2733b885007b5a24615df444ce8735b7406d
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2022-02-24 09:29:06 +0100

    E5 parte 1

--- src/e5_oneSem_Killer/E5_SemWithKiller.java
+++ src/e5_oneSem_Killer/E5_SemWithKiller.java
@@ -1,4 +1,4 @@
- package e5_oneSem_Killer;
+package e5_oneSem_Killer;
 
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
@@ -13,14 +13,14 @@ public class E5_SemWithKiller {
 		Bang[] theBangs = new Bang[HOW_MANY];
 
 		Synchronizer sync = new Synchronizer(HOW_MANY);
-		
+
 		SerialBangKiller dexter = new SerialBangKiller(sync, theBangs);
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("\nPress return to start the killing spree");
 		System.out.println("Then press return again to put an end to this nightmare ");
 		sc.nextLine();
-		
+
 		for (int i = 0; i < HOW_MANY; i++) {
 			thePings[i] = new Ping(i, sync);
 			thePongs[i] = new Pong(i, sync);
@@ -29,122 +29,164 @@ public class E5_SemWithKiller {
 			thePongs[i].start();
 			theBangs[i].start();
 		}
-		
+
 		dexter.start();
-		
-		sc.nextLine ();
-		
+
+		sc.nextLine();
+
 		System.exit(0);
 	}
 }
 
-
 class Synchronizer {
-	
+
 	private final int numPongs; // number of pong instances
-	
+
 	/* declare your primitive-typed variables and constants here */
-	
+
+	private volatile int REMAINING_PING = 1;
+	private volatile int REMAINING_PONG = 0;
+	private volatile int REMAINING_BANG = 0;
+
+	private int nextId = 0;
+
 	/* COMPLETE */
-	
+
 	Semaphore mutex = new Semaphore(1);
-	
+
 	// Constructor
-	public Synchronizer (int numPongs) {
+	public Synchronizer(int numPongs) {
 		this.numPongs = numPongs;
 	}
-	
+
 	public void letMePing() {
 		/* COMPLETE */
+		while (REMAINING_PING <= 0 || !mutex.tryAcquire()) {
+			Thread.yield();
+		}
 	}
-	
-	public  void letMePong(int id) {
+
+	public void letMePong(int id) {
 		/* COMPLETE */
+		while (id != nextId)
+			Thread.yield();
+
+		while (REMAINING_PONG <= 0 || !mutex.tryAcquire()) {
+			Thread.yield();
+		}
 	}
-	
+
 	public void letMeBang() {
 		/* COMPLETE */
+		while (REMAINING_BANG <= 0 || !mutex.tryAcquire()) {
+			Thread.yield();
+		}
 	}
 
 	public void pingDone() {
 		/* COMPLETE */
+		REMAINING_PING = 0;
+		REMAINING_PONG = 2;
+		mutex.release();
 	}
 
 	public void pongDone(int id) {
 		/* COMPLETE */
+		nextId = (id + 1) % numPongs;
+		REMAINING_PONG--;
+		if (REMAINING_PONG <= 0)
+			REMAINING_BANG = 1;
+		mutex.release();
 	}
-	
+
 	public void bangDone() {
 		/* COMPLETE */
+		REMAINING_BANG = 0;
+		REMAINING_PING = 1;
+		mutex.release();
 	}
-	
+
 	public void letMeKill() {
 		/* COMPLETE */
 	}
-	
+
 	public void killingDone() {
 		/* COMPLETE */
 	}
-	
+
 }
 
 class SerialBangKiller extends Thread {
-	
+
 	private Synchronizer synchronizer;
 	private int nextBangToKill = 0;
 	private Bang[] theBangs;
-	
-	public SerialBangKiller (Synchronizer synchronizer, Bang[] theBangs) {
+
+	public SerialBangKiller(Synchronizer synchronizer, Bang[] theBangs) {
 		this.synchronizer = synchronizer;
 		this.theBangs = theBangs;
 	}
-	
-	public void run () {
+
+	public void run() {
 		while (true) {
 			/* COMPLETE */
 		}
 	}
 }
 
-
-/* COMPLETE and modify class Bang to make its
- * instances stoppable */
+/*
+ * COMPLETE and modify class Bang to make its
+ * instances stoppable
+ */
 
 class Bang extends Thread {
 	private int id;
 	private Synchronizer synchronizer;
-	
-	
-	public Bang (int id, Synchronizer synchronizer) {
+	private volatile boolean alive;
+
+	public Bang(int id, Synchronizer synchronizer) {
 		this.synchronizer = synchronizer;
 		this.id = id;
+		this.alive = true;
 	}
-	
-	public void syncStop () {
+
+	public void syncStop() {
 		/* COMPLETE */
+		alive = false;
 	}
-	
-	public void run () {
+
+	public void run() {
 		/* COMPLETE */
+		while (alive) {
+			synchronizer.letMeBang();
+			System.out.println("BANG!(" + id + ")");
+			synchronizer.bangDone();
+			try {
+				Thread.sleep(25);
+			} catch (InterruptedException ie) {
+			}
+		}
 	}
 }
 
-
-/* Classes Ping and Pong are complete. 
- * DO NOT MODIFY THEM */
+/*
+ * Classes Ping and Pong are complete.
+ * DO NOT MODIFY THEM
+ */
 
 class Ping extends Thread {
 	private int id;
 	private Synchronizer synchronizer;
-	
-	public Ping (int id, Synchronizer synchronizer) {
+
+	public Ping(int id, Synchronizer synchronizer) {
 		this.synchronizer = synchronizer;
-		this.id = id;}
-	
-	public void run () {
+		this.id = id;
+	}
+
+	public void run() {
 		while (true) {
 			synchronizer.letMePing();
-			System.out.print("PING("+id+") ");
+			System.out.print("PING(" + id + ") ");
 			synchronizer.pingDone();
 		}
 	}
@@ -153,18 +195,17 @@ class Ping extends Thread {
 class Pong extends Thread {
 	private int id;
 	private Synchronizer synchronizer;
-	
-	public Pong (int id, Synchronizer synchronizer) {
+
+	public Pong(int id, Synchronizer synchronizer) {
 		this.synchronizer = synchronizer;
-		this.id = id;}
-	
-	public void run () {
+		this.id = id;
+	}
+
+	public void run() {
 		while (true) {
 			synchronizer.letMePong(id);
-			System.out.print("pong("+id+") ");
+			System.out.print("pong(" + id + ") ");
 			synchronizer.pongDone(id);
 		}
 	}
 }
-
-
