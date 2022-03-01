ae4a5948d7329e530986bffa3f8a30a548f202a2
Guillem Poy <guplem@gmail.com>
2022-03-01 12:48:04 +0100

    Basic GUI

---
+++ src/e6_Psychedelia/E6_Psychedelia.java
@@ -0,0 +1,65 @@
+package e6_Psychedelia;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class E6_Psychedelia {
+	public static void main(String args[]) {
+		JFrame frame = new JFrame("My First GUI");
+		// frame.setLayout(new FlowLayout());
+		frame.setLayout(new GridLayout(2, 0));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(450, 450);
+		frame.add(new PsychedelicLabel());
+		frame.add(new PsychedelicLabel());
+		frame.add(new PsychedelicLabel());
+		JSlider slider = new JSlider();
+		slider.addChangeListener(new SliderListener());
+		frame.add(slider);
+		JButton btnGO = new JButton("GO");
+		btnGO.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("GO");
+			}
+		});
+		frame.add(btnGO);
+		JButton btnSUSPEND = new JButton("SUSPEND");
+		btnSUSPEND.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("SUSPEND");
+			}
+		});
+		frame.add(btnSUSPEND);
+		JButton btnRESUME = new JButton("RESUME");
+		btnRESUME.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("RESUME");
+			}
+		});
+		frame.add(btnRESUME);
+		JButton btnSTOP = new JButton("STOP");
+		btnSTOP.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("STOP");
+			}
+		});
+		frame.add(btnSTOP);
+		frame.setVisible(true);
+	}
+}
+
+class SliderListener implements ChangeListener {
+	public void stateChanged(ChangeEvent e) {
+		JSlider source = (JSlider) e.getSource();
+		if (!source.getValueIsAdjusting()) {
+			int value = (int) source.getValue();
+			System.out.println("New Slider Value: " + value);
+		}
+	}
+}
